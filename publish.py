1086cc73085ac6e1e4f89512af33fe07de244b6a
Icosahunter <anzelknight@gmail.com>
2025-01-22 21:59:51 -0600

    First commit!

---
+++ publish.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+from re import X
+import shutil
+
+copy_globs = ['*.css', '*.js']
+page_globs = ['**/*.txt']
+template = Path('template.html')
+src = Path('src/')
+dist = Path('dist/')
+
+if dist.exists():
+    shutil.rmtree(dist)
+
+to_copy = []
+
+for x in copy_globs:
+    to_copy.extend(src.glob(x))
+
+pages = []
+
+for x in page_globs:
+    pages.extend(src.glob(x))
+
+dist.mkdir(exist_ok=True)
+
+for x in to_copy:
+    dest = dist / x.relative_to(src)
+    dest.parent.mkdir(exist_ok=True)
+    shutil.copy(x, dest)
+
+t = ''
+with open(src / template, 'r') as f:
+    t = f.read()
+
+for x in pages:
+    page = ''
+    dest = dist / x.relative_to(src).with_suffix(template.suffix)
+    dest.parent.mkdir(exist_ok=True)
+    with open(x, 'r') as f:
+        page = t.format(content=f.read())
+    with open(dest, '+w') as f:
+        f.write(page)
