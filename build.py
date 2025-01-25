c6c6fb38c7fb46a5cd8eb7f227e061736cf95b4e
Icosahunter <anzelknight@gmail.com>
2025-01-25 14:55:08 -0600

    Fix build script

--- build.py
+++ build.py
@@ -41,7 +41,7 @@ for x in pages:
     dest = dist / x.relative_to(src).with_suffix('') / 'index.html'
     if x == home:
         dest = dist / 'index.html'
-    os.makedirs(dest, exist_ok=True)
+    os.makedirs(dest.parent, exist_ok=True)
     with open(x, 'r') as f:
         page = t.format(content=f.read())
     with open(dest, '+w') as f:
